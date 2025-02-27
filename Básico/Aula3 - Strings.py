706e4727da925d0fe63b70fd7cf892d7aa0a12f4
Daniel da Costa Mendes <145233130+nielcosta@users.noreply.github.com>
2025-02-27 19:33:17 -0300

    Add files via upload

---
+++ Básico/Aula3 - Strings.py
@@ -0,0 +1,21 @@
+""" 
+Python - Linguagem de Programação 
+Tipagem - Dinâmica Forte
+str -> string > Tipo texto
+Strings são textos que estão dentro de aspas 
+Ex: print('String') 
+
+"""
+
+# Aspas simples 
+print('Olá Mundo!')
+print('Olá "Daniel"!')
+
+# Aspas duplas
+print("Olá Mundo!")
+print("Olá 'Daniel'!")
+
+# Escape
+print("Olá \" Velho Mundo!\"")
+#r 
+print(r"Olá \" Velho Mundo!\"")
