4d7d8b6acfb283abaed393c5007bfc658c015574
Daniel da Costa Mendes <145233130+nielcosta@users.noreply.github.com>
2025-02-27 19:12:48 -0300

    Add files via upload

---
+++ Básico/Aula 2 - Print.py
@@ -0,0 +1,13 @@
+""" 
+Função Print  - Imprime um resultado na tela 
+
+"""
+
+# Função CRLF
+""" Sep - O que separa os elementos 
+    end - O que finaliza o final do print
+"""
+
+
+print (1,2,3,sep="-", end='-')
+print(4,5,6,sep='-')
