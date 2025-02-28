351dee68bfbf7ff39f49849f2488385957dbbb86
Daniel da Costa Mendes <145233130+nielcosta@users.noreply.github.com>
2025-02-28 16:11:07 -0300

    Add files via upload

---
+++ Básico/Aula4_Int_e_Float .py
@@ -0,0 +1,37 @@
+#Int e Float 
+
+""" 
+int - Número inteiro sem casas decimais. 
+Representa qualquer número positivo ou negativo. 
+Int sem sinal é considerado número positivo . 
+
+"""
+print (1,2,3,4)
+print (type(13))
+print(-3,-2,-1)
+print (type(-32))
+
+""" Float 
+Float - é um número com casas decimais ou ponto flutuante. 
+Pode ser um número positivo ou negativo com casas decimais. 
+Float sem sinal é considerado número positivo. 
+
+"""
+print (1.2,3.4,5.03)
+print (type(.03))
+print(-3.2,-1.9)
+print (type(-3.2))
+
+
+
+""" Função Type 
+    Mostra o tipo de valor fornecido. 
+"""
+print (type(0))
+print (type(-3.2))
+
+""" Função Bool ou Boleana
+    Valor verdadeiro ou falso de uma sentença. 
+"""
+print (bool(10>3))
+print (bool(""))
