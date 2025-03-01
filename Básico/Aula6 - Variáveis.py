c208d8270fd97ca18796c1db5c9b1fcea53b75a9
Daniel da Costa Mendes <145233130+nielcosta@users.noreply.github.com>
2025-03-01 15:03:03 -0300

    Add files via upload

---
+++ Básico/Aula6 - Variáveis.py
@@ -0,0 +1,23 @@
+""" 
+Variáveis 
+São usadas para salvar algum valor ou conteúdo na memória do Computador. 
+#PEP8 : Iniciar variváeis com letras minúsculas
+Pode usar números e Underline . 
+O sinal de '=' é o operador de atribuição . Ele é usado para atribuir um valor a variável. 
+Uso: nome_variável = <expressão>
+
+
+nome = 'Daniel Mendes'
+print(nome)
+soma2 =2+2
+print(soma2)
+int_um = int('1')
+print(int_um,type(int_um))
+
+"""
+nome = 'Daniel'
+idade = 18
+maior_idade = idade >=18
+
+if (maior_idade == True) : print(f"{nome} possui {idade} de idade.Ele é maior de idade.")
+else: print(f"{nome} possui {idade} de idade.Ele é menor de idade.")
