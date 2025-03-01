91cf8cb4191a7e2c7a4f70f3e24a890199c0f317
Daniel da Costa Mendes <145233130+nielcosta@users.noreply.github.com>
2025-03-01 14:35:53 -0300

    Add files via upload

---
+++ Básico/Aula5 - Conversao.py
@@ -0,0 +1,28 @@
+""" 
+Conversão 
+Conversão de Tipos ou Coerção 
+é o ato de converter um tipo de caracter em outro tipo. 
+Tipos primitivos : Inteiro, Float e String e Boleano. 
+
+
+"""
+#String 
+print('1')
+print(type('1'))
+
+
+#String  para Inteiro 
+print (int('1'))
+print(type(int('1')))
+
+#String  para Float 
+print (float('1'))
+print(type(float('1')))
+
+#Inteiro para String 
+print (str(1))
+print(type(str(1)))
+
+#String para Boleano
+print (bool('1'))
+print(type(bool('1')))
